46798f0cae77f8060d4c995878606dccc99f2ddb
Fabian Altermann <F.Altermann@gmx.net>
2024-12-01 12:13:48 +0100

    Day 1: Refactor code for main() function

--- 01/part1.py
+++ 01/part1.py
@@ -1,17 +1,23 @@
 import csv
 
-with open("./data.txt", "r") as file:
-    reader = csv.reader(file, delimiter=" ", skipinitialspace=True)
-    list1 = []
-    list2 = []
 
-    for line in reader:
-        list1.append(int(line[0]))
-        list2.append(int(line[1]))
+def main() -> None:
+    with open("./data.txt", "r") as file:
+        reader = csv.reader(file, delimiter=" ", skipinitialspace=True)
+        list1 = []
+        list2 = []
 
-list1.sort()
-list2.sort()
+        for line in reader:
+            list1.append(int(line[0]))
+            list2.append(int(line[1]))
 
-result_list = [abs(list2[i] - list1[i]) for i in range(len(list1))]
+    list1.sort()
+    list2.sort()
 
-print(f"{sum(result_list)}")
+    result_list = [abs(list2[i] - list1[i]) for i in range(len(list1))]
+
+    print(sum(result_list))
+
+
+if __name__ == "__main__":
+    main()
--- 01/part2.py
+++ 01/part2.py
@@ -1,16 +1,21 @@
 import csv
 from collections import Counter
 
-with open("./data.txt", "r") as file:
-    reader = csv.reader(file, delimiter=" ", skipinitialspace=True)
-    list1 = []
-    list2 = []
 
-    for line in reader:
-        list1.append(int(line[0]))
-        list2.append(int(line[1]))
+def main() -> None:
+    with open("./data.txt", "r") as file:
+        reader = csv.reader(file, delimiter=" ", skipinitialspace=True)
+        list1 = []
+        list2 = []
+
+        for line in reader:
+            list1.append(int(line[0]))
+            list2.append(int(line[1]))
+
+    count = Counter(list1)
+
+    print(sum([count[i] * i for i in list2]))
 
 
-count = Counter(list1)
-
-print(sum([count[i] * i for i in list2]))
+if __name__ == "__main__":
+    main()
